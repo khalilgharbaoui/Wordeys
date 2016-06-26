9e95d8be7d72cd26abe5bdeaf6d568694ff6fc47
Chiel Hackman <chielhackman@gmail.com>
2016-06-26 13:30:37 +0200

    Heroku fix

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module Wordeys
       end
     end
 
+    config.assets.initialize_on_precompile = false
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
